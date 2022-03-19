b4fd495b5656cdea20c1fc88bc6330bbc4355686
EuphoriaForever <smiiiileeeey11@gmail.com>
2022-03-19 10:14:34 +0800

    FPI works na

--- FPI.R
+++ FPI.R
@@ -1,7 +1,13 @@
 library(NLRoot)
+library(spuRs)
+library("SciViews")
 
 func <- function(x) {
-  (exp(1))^x + x - 6
+  exp(1)^x + x - 6
+}
+
+func1 <- function(x) {
+  ln(6-x)
 }
 
 curve(func, xlim=c(-3,3), col='blue', lwd=1.5, lty=2)
@@ -9,21 +15,23 @@ abline(h=0)
 abline(v=0)
 uniroot(func,c(1,2))
 
-fixedpoint <- function(fun, x, tol=1e-07, n=500){
+
+fixedpoint2 <- function(fun, x0, tol=1e-07, niter=500){
   ## fixed-point algorithm to find x such that fun(x) == x
   ## assume that fun is a function of a single variable
   ## x0 is the initial guess at the fixed point
   
-  xold <- x
+  xold <- x0
   xnew <- fun(xold)
-  for (i in 1:n) {
+  for (i in 1:niter) {
     xold <- xnew
     xnew <- fun(xold)
-    if ( abs((xnew-xold)) < tol ){
-      return(xnew) 
-    }
+    if ( abs((xnew-xold)) < tol )
+      return(xnew)
   }
   stop("exceeded allowed number of iterations")
 }
 
-fixedpoint(func, 1)
+fixedpoint(func1, 1, tol = 1e-6)
+
+fixedpoint2(func1,1)
